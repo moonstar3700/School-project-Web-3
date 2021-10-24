eaf135d6d3fd4413ae06f93307ab181f509e70ac
Patryk Piekarz <patryk.piekarz@student.ucll.be>
2021-10-24 17:56:10 +0200

    training update 4

---
+++ src/main/java/ui/controller/ToEditTraining.java
@@ -0,0 +1,12 @@
+package ui.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ToEditTraining extends RequestHandler{
+
+    @Override
+    public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+        return null;
+    }
+}
