0c875193dfc3d35aed273c01746571ef4e1e9625
Patryk Piekarz <patryk.piekarz@student.ucll.be>
2021-12-06 14:39:29 +0100

    delete fix

--- src/main/java/ui/controller/Delete.java
+++ src/main/java/ui/controller/Delete.java
@@ -4,6 +4,7 @@ import domain.model.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class Delete extends RequestHandler{
@@ -18,6 +19,11 @@ public class Delete extends RequestHandler{
             return "Controller?command=UserOverview";
         }
         service.delete(userid);
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user.getUserid() == userid){
+            session.invalidate();
+        }
         try {
             response.sendRedirect("Controller?command=UserOverview&confirmation=succesDelete");
         } catch (IOException e) {
