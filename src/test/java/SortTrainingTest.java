00bb26237c0142700c905df2deea33dd599155cb
Sarah Reyskens <sarah.reyskens@student.ucll.be>
2021-12-06 15:04:21 +0100

    laatste test aanpassingen

--- src/test/java/SortTrainingTest.java
+++ src/test/java/SortTrainingTest.java
@@ -17,11 +17,11 @@ public class SortTrainingTest {
         //System.setProperty("webdriver.chrome.driver", "/Users/.../web3pers/chromedriver");
         // windows: gebruik dubbele \\ om pad aan te geven
         // hint: zoek een werkende test op van web 2 ...
-        //System.setProperty("webdriver.chrome.driver", "C:/Users/Sarah/Toegepaste Informatica/1ste Fase/Webontwikkeling 2/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "D:\\informatica cursus\\IT 2de jaar\\Web 3\\chrome driver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Sarah/Toegepaste Informatica/1ste Fase/Webontwikkeling 2/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "D:\\informatica cursus\\IT 2de jaar\\Web 3\\chrome driver\\chromedriver.exe");
         driver = new ChromeDriver();
-        //driver.get("http://localhost:8080/Groep1_17_war_exploded/registertraining.jsp");
-        driver.get("http://localhost:8080/Groep1_17_war2/registertraining.jsp");
+        driver.get("http://localhost:8080/Groep1_17_war_exploded/registertraining.jsp");
+        //driver.get("http://localhost:8080/Groep1_17_war2/registertraining.jsp");
     }
 
     @After
