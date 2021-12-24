2d2f68f8538a9f54eac04dba5b68bb161d287283
Patryk Piekarz <patryk.piekarz@student.ucll.be>
2021-12-24 20:24:45 +0100

    update authenticatie delete

--- src/main/java/ui/controller/ConfirmDelete.java
+++ src/main/java/ui/controller/ConfirmDelete.java
@@ -1,14 +1,24 @@
 package ui.controller;
 
+import domain.model.Role;
 import domain.model.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ConfirmDelete extends RequestHandler{
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
         int userid;
+
+        HttpSession session = request.getSession();
+        User log = (User) session.getAttribute("user");
+        if (log == null){
+            request.setAttribute("errors", "u moet ingelogd zijn om users te verwijderen");
+            return "index.jsp";
+        }
+
         try {
             userid = Integer.parseInt(request.getParameter("userid"));
         } catch (NumberFormatException e) {
@@ -16,9 +26,14 @@ public class ConfirmDelete extends RequestHandler{
             return "Controller?command=UserOverview";
         }
 
+        User user = service.get(userid);
+        if (log.getRole() == Role.ADMIN && log.getUserid() != user.getUserid() || log.getRole() == Role.COORDINATOR && user.getGroup() != log.getGroup()){
+            request.setAttribute("errors", "U heeft geen authenticatie om deze user te verwijderen");
+            return "Controller?command=UserOverview";
+        }
+
         request.setAttribute("userid", userid);
         try {
-            User user = service.get(userid);
             String firstName = user.getFirstName();
             request.setAttribute("firstName", firstName);
             String lastName = user.getLastName();
